7e3e8dba95525d88332893c754e5da635716119f
Damian Kubiak <kubiak.damian96@gmail.com>
2018-06-04 03:21:27 +0200

    Working app v1

--- src/main/java/pl/quizujemy/back/controllers/AnteroomController.java
+++ src/main/java/pl/quizujemy/back/controllers/AnteroomController.java
@@ -26,6 +26,7 @@ public class AnteroomController {
     }
 
     @DeleteMapping("anteroom/delete/{idanteroom}")
+    @CrossOrigin(origins = "*")
     public ResponseEntity<?> deleteAnteroom(@PathVariable Long idanteroom) {
         return anteroomRepository.findById(idanteroom).map(anteroom -> {
             anteroomRepository.delete(anteroom);
